50a47bb2403701ae234cc348cda9c1688b3438d5
Eagle Raven Global <grant@glendevonmarine.com>
2024-10-24 10:29:02 +1300

    Update Headings, Buttons
    Testing if JS will change the content of headings, buttons.

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 console.log("Test")
+
+// Welcome Back Header
+const welcomeBack = document.getElementById("ctl00_ContentPlaceHolder1_lblWelcomeText"); 
+welcomeBack.innerHTML = "Welcome back to Eagle Raven Global! Let's get your shipment taken care of.";
+
+// Sign in Button
+const signInbtn = document.getElementById("ctl00_ContentPlaceHolder1_cmdLogin");
+signInbtn.value = "Sign In";
+
+// Create Account Button
+const createAccountbtn = document.getElementById("ctl00_ContentPlaceHolder1_btnCreateAccount");
+createAccountbtn.value = "Create Account";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
